45f49c0a47560c3dd92194d1eddb410162508f5c
ben-rhuman <benrhuman@172.17.7.245>
2016-10-15 15:13:20 -0600

    IE 10

--- WumpusWorld/src/InferenceEnginePackage/InferenceEngine.java
+++ WumpusWorld/src/InferenceEnginePackage/InferenceEngine.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package InferenceEnginePackage;
+
 import WumpusWorld.*;
 import KnowledgeBasePackage.*;
 import java.util.ArrayList;
@@ -13,33 +14,30 @@ import java.util.ArrayList;
  * @author benrhuman
  */
 public class InferenceEngine {
-    
+
     private KnowledgeBase KB;
     private Location agentLocation;
     private int agentDirection;
-    
-    public InferenceEngine(int size){
+
+    public InferenceEngine(int size) {
         KB = new KnowledgeBase(size);
     }
-    
-    public void TELL(boolean[] percept, Location location, int direction){  // Allows tha agent to tell the IE about its percepts
-      agentLocation = location;
-      agentDirection = direction;
-      
-      updateKnowledgeBase(percept);
+
+    public void TELL(boolean[] percept, Location location, int direction) {  // Allows tha agent to tell the IE about its percepts
+        agentLocation = location;
+        agentDirection = direction;
+
+        updateKnowledgeBase(percept);
     }
-    
-    public ArrayList ASK(){  //Allows the IE to tell the agent a list of moves to take
-        
-        
+
+    public ArrayList ASK() {  //Allows the IE to tell the agent a list of moves to take
+
     }
-    
-    
-    private ArrayList PLAN(){
-        
+
+    private ArrayList PLAN() {
+
     }
-    
-    
+
 //(0)Move suceeded (True) move failed/wall (False)
 //(1)New room obstacle
 //(2)New room is breezy room 
@@ -47,9 +45,9 @@ public class InferenceEngine {
 //(4)New room is pit
 //(5)New room is wumpus
 //(6)New room is gold
-    
-    public void updateKnowledgeBase(boolean[] percept){
+    public void updateKnowledgeBase(boolean[] percept) {
         
+        KB.KBMap[agentLocation.i][agentLocation.j].unknown = false;
         //Used for code readablity
         boolean obstacle = percept[1];
         boolean breezy = percept[2];
@@ -57,29 +55,43 @@ public class InferenceEngine {
         boolean pit = percept[4];
         boolean wumpus = percept[5];
         boolean glitter = percept[6];
-        
-        
-        if(obstacle){   //If the agent hits an obstacle we need to correct for the fact that that the agentLocation is deferent from the location of the space we our updating
-            KB.setObstacle(agentLocation.i, agentLocation.j);
-        }else if(pit){
+
+        if (obstacle) {   //If the agent hits an obstacle we need to correct for the fact that that the agentLocation is deferent from the location of the space we our updating
+            int xMod = 0;
+            int yMod = 0;
+            if (agentDirection == 1) {
+                yMod = -1;
+            } else if (agentDirection == 2) {
+                xMod = 1;
+            } else if (agentDirection == 3) {
+                yMod = 1;
+            } else {
+                xMod = -1;
+            }
+            KB.KBMap[agentLocation.i + xMod][agentLocation.j + yMod].breeze = false;
+            KB.setObstacle(agentLocation.i + xMod, agentLocation.j + yMod);
+        } else if (pit) {
             KB.setKnownPit(agentLocation.i, agentLocation.j);   //Agent is dead, but we still need to update the map
-        }else if(wumpus){
+        } else if (wumpus) {
             KB.setKnownWumpus(agentLocation.i, agentLocation.j);   //Agent is dead, still need to update map.
-        }else if(breezy && stinky){
+        } else if (breezy && stinky) {
             KB.KBMap[agentLocation.i][agentLocation.j].breeze = true;
             KB.KBMap[agentLocation.i][agentLocation.j].stench = true;
-        } else if(breezy){
+        } else if (breezy) {
             KB.KBMap[agentLocation.i][agentLocation.j].breeze = true;
-        }else if(stinky){
+        } else if (stinky) {
             KB.KBMap[agentLocation.i][agentLocation.j].stench = true;
-        }else{ //Determines whether or not space can be marked as safe.
+        } else { //Determines whether or not space can be marked as safe.
             KB.setSafe(agentLocation.i, agentLocation.j);
         }
-        
-        if(glitter){
+
+        if (glitter) {
             KB.KBMap[agentLocation.i][agentLocation.j].glitter = true;
         }
     }
-    
+
+    private void LocicalMapDeduction(){
+        
+    }
     
 }
