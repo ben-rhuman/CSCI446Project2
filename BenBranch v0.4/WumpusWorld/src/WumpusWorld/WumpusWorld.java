ca20a8641e74324508fad659ed7df4ca59418008
Ben Rhuman <benrhuman@gmail.com>
2016-10-19 12:11:08 -0600

    Add files via upload

---
+++ BenBranch v0.4/WumpusWorld/src/WumpusWorld/WumpusWorld.java
@@ -0,0 +1,26 @@
+package WumpusWorld;
+/**
+ * Wumpus World using First Order Logic
+ * Ben Rhuman, Danny Kumpf, Isaac Sotelo
+ */
+
+import java.util.Scanner;
+
+public class WumpusWorld {
+
+    public static void main(String[] args) {
+        int size = 5;
+        double pitP = .05;
+        double obstacleP = .05;
+        double wumpusP = .05;
+
+        
+        Map m = new Map(size, pitP, obstacleP, wumpusP);
+        m.print();
+        
+        Agent a =  new Agent(0, 0, 1, 2, m);
+        
+
+    }
+
+}
